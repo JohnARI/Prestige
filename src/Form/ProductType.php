ab22c7b493046f766b0138e9c0253f3bed5af6f2
JohnARI <90911153+JohnARI@users.noreply.github.com>
2021-12-30 21:35:42 +0100

    modification home

--- src/Form/ProductType.php
+++ src/Form/ProductType.php
@@ -23,25 +23,12 @@ class ProductType extends AbstractType
                 'attr' => ['placeholder' => 'Entrez le nom du produit']
             ])
 
-            ->add('dialColor', TextType::class, [
-                'label' => 'Produit',
-                'attr' => ['placeholder' => 'Entrez la couleur du cadran']
-            ])
-
             ->add('movement', TextType::class, [
                 'label' => 'Produit',
                 'attr' => ['placeholder' => 'Entrez le type de mouvement']
             ])
 
-            ->add('matter', TextType::class, [
-                'label' => 'Produit',
-                'attr' => ['placeholder' => 'Entrez lz matière']
-            ])
 
-            ->add('length', IntegerType::class, [
-                'label' => 'Produit',
-                'attr' => ['placeholder' => 'Entrez la longueur']
-            ])
 
             ->add('braceletType', TextType::class, [
                 'label' => 'Produit',
